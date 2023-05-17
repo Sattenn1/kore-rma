dd1ec8ce11462e84bc23e401108db5f73aa2dae1
Edita <1.edita.stankeviciute@gmail.com>
2023-05-17 22:07:03 +0300

    fix settings

--- rma/settings.py
+++ rma/settings.py
@@ -18,6 +18,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
 STATIC_DIR = os.path.join(BASE_DIR, "static")
 
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
